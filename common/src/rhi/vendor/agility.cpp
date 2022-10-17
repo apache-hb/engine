2250f915aebaac1da57d3ebd6c35545c9d1b2030
Elliot <35050275+apache-hb@users.noreply.github.com>
2022-10-17 10:22:07 -0400

    move stuff around

--- common/src/rhi/vendor/agility.cpp
+++ common/src/rhi/vendor/agility.cpp
@@ -3,5 +3,5 @@
 extern "C" { 
     // load up the agility sdk
     DLLEXPORT extern const UINT D3D12SDKVersion = 600; 
-    DLLEXPORT extern const auto* D3D12SDKPath = u8".\\D3D12\\"; 
+    DLLEXPORT extern const auto* D3D12SDKPath = u8".\\resources\\agility"; 
 }
