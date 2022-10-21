9000c32451ce0714b3f9ce5249aee3d41fa23b74
Elliot <35050275+apache-hb@users.noreply.github.com>
2022-10-21 16:12:55 -0400

    sponza

--- game/main.cpp
+++ game/main.cpp
@@ -142,7 +142,7 @@ int commonMain() {
     });
     
     render::Perspective camera { { 1.f, 1.f, 1.f }, { 1.f, 1.f, 1.f }, 110.f };
-    auto world = assets::loadGltf("D:\\assets\\amongus\\amongus\\amongus.gltf");
+    auto world = assets::loadGltf("D:\\assets\\glTF-Sample-Models-master\\2.0\\Sponza\\glTF\\Sponza.gltf");
 
     render::BasicScene scene { { &camera, &world } };
 
