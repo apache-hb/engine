fdb773964419483461e132ef36d3d0c719776dee
Elliot <35050275+apache-hb@users.noreply.github.com>
2023-01-01 23:39:50 +0000

    extract common logic out

--- common/include/engine/memory/bitmap.h
+++ common/include/engine/memory/bitmap.h
@@ -3,10 +3,40 @@
 #include "engine/base/container/unique.h"
 #include <stdint.h>
 
-// TODO: extract logic to common base class
-
 namespace simcoe::memory {
-    struct BitMap final {
+    namespace detail {
+        template<typename T>
+        struct Storage {
+            constexpr Storage(size_t bits): size(bits), data(new T[wordCount()]) { 
+                reset();
+            }
+
+            constexpr size_t getSize() const { return size; }
+
+        protected:
+            const size_t size;
+            UniquePtr<T[]> data;
+
+            constexpr T getMask(size_t bit) const { return T(1) << (bit % kBits); }
+            constexpr size_t getWord(size_t bit) const { return bit / kBits; }
+
+        private:
+            constexpr static inline size_t kBits = sizeof(T) * CHAR_BIT;
+
+            constexpr size_t wordCount() const { 
+                return getSize() / kBits + 1;
+            }
+
+            void reset() {
+                std::fill_n(data.get(), wordCount(), 0);
+            }
+        };
+    }
+
+    struct BitMap final : detail::Storage<uint64_t> {
+        using Super = detail::Storage<uint64_t>;
+        using Super::Super;
+
         BitMap(size_t size);
 
         size_t alloc(size_t count = 1);
@@ -20,12 +50,12 @@ namespace simcoe::memory {
         void setRange(size_t start, size_t length);
         void setBit(size_t bit);
         void clearBit(size_t bit);
-
-        const size_t size;
-        UniquePtr<uint64_t[]> bits;
     };
 
-    struct AtomicBitMap final {
+    struct AtomicBitMap final : detail::Storage<std::atomic_uint64_t> {
+        using Super = detail::Storage<std::atomic_uint64_t>;
+        using Super::Super;
+
         AtomicBitMap(size_t size);
 
         size_t alloc();
@@ -39,8 +69,5 @@ namespace simcoe::memory {
 
         // set a bit and return true if it was previously clear
         bool testSetBit(size_t bit);
-
-        const size_t size;
-        UniquePtr<std::atomic_uint64_t[]> bits;
     };
 }
--- common/src/memory/bitmap.cpp
+++ common/src/memory/bitmap.cpp
@@ -2,19 +2,7 @@
 
 using namespace simcoe::memory;
 
-namespace {
-    // TODO: this assumes 64-bit elements
-    constexpr size_t wordCount(size_t bits) {
-        return bits / 64 + 1;
-    }
-}
-
-BitMap::BitMap(size_t size)
-    : size(size)
-    , bits(new uint64_t[wordCount(size)])
-{ 
-    std::fill_n(bits.get(), wordCount(size), 0);
-}
+BitMap::BitMap(size_t bits) : Super(bits) { }
 
 size_t BitMap::alloc(size_t count) {
     ASSERT(count > 0);
@@ -46,21 +34,16 @@ void BitMap::setRange(size_t start, size_t length) {
 bool BitMap::testBit(size_t bit) const {
     ASSERT(bit < size);
 
-    return bits[bit / 64] & (1ull << (bit % 64));
+    return data[getWord(bit)] & getMask(bit);
 }
 
 void BitMap::setBit(size_t bit) {
     ASSERT(bit < size);
 
-    bits[bit / 64] |= (1ull << (bit % 64));
+    data[getWord(bit)] |= getMask(bit);
 }
 
-AtomicBitMap::AtomicBitMap(size_t size)
-    : size(size)
-    , bits(new std::atomic_uint64_t[wordCount(size)])
-{ 
-    std::fill_n(bits.get(), wordCount(size), 0);
-}
+AtomicBitMap::AtomicBitMap(size_t bits) : Super(bits) { }
 
 size_t AtomicBitMap::alloc() {
     for (size_t i = 0; i < size; i++) {
@@ -79,25 +62,25 @@ void AtomicBitMap::release(size_t index) {
 bool AtomicBitMap::testBit(size_t bit) const {
     ASSERT(bit < size);
 
-    return bits[bit / 64] & (1ull << (bit % 64));
+    return data[getWord(bit)] & getMask(bit);
 }
 
 void AtomicBitMap::setBit(size_t bit) {
     ASSERT(bit < size);
 
-    bits[bit / 64] |= (1ull << (bit % 64));
+    data[getWord(bit)] |= getMask(bit);
 }
 
 void AtomicBitMap::clearBit(size_t bit) {
     ASSERT(bit < size);
 
-    bits[bit / 64] &= ~(1ull << (bit % 64));
+    data[getWord(bit)] &= ~getMask(bit);
 }
 
 bool AtomicBitMap::testSetBit(size_t bit) {
     ASSERT(bit < size);
 
-    const uint64_t mask = 1ull << (bit % 64);
+    const uint64_t mask = getMask(bit);
 
-    return bits[bit / 64].fetch_or(mask) & mask;
+    return data[getWord(bit)].fetch_or(mask) & mask;
 }
